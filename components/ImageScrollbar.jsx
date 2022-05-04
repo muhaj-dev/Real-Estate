c3382bdf67d36f832e8eedfb70777edaf038b4c0
muhammod1 <muhammodholawale@gmail.com>
2022-05-04 07:06:59 +0100

    refactors

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -44,8 +44,8 @@ const RightArrow = () => {
 export default function ImageSrollbar({ data }) {
     return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{overflow: "hidden"}} >
-        {data.map((item) => (
-            <Row className="p-1 overflow-hidden" style={{width: '910px'}} itemId={item.id}>
+        {data.map((item,key) => (
+            <Row className="p-1 overflow-hidden" key={key} style={{width: '910px'}} itemId={item.id}>
                <Image 
                 placeholder="blur" 
                 blurDataURL={item.url} 
